6fef4d28f9e52f64c064bc3bb40c689993f93f24
Andrew Nguyen <andrew-git@na-consulting.net>
2014-03-06 20:20:29 -0800

    Initial creation of repos

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cljogger
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/cljogger/core_test.clj
@@ -0,0 +1,7 @@
+(ns cljogger.core-test
+  (:require [clojure.test :refer :all]
+            [cljogger.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
